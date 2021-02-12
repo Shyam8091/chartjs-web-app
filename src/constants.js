896cef39e6268c556913f9e0a5adfa4aff5313a4
Shyam8091 <Shyam.soni8091@gmail.com>
2021-02-12 21:47:16 +0530

    adding .env.local file

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,1 @@
-/**
- * @author Shyam Soni
- * @description 
- * export const BASE_URL =  'http://localhost:5000'; --For local
- */
-export const BASE_URL =  'https://chartjs-backend-app.herokuapp.com/';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
