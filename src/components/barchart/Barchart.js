fb1bd1601096207b572aa5b766d67dad62adaea5
Shyam8091 <Shyam.soni8091@gmail.com>
2021-02-13 17:41:59 +0800

    changing favicon

--- src/components/barchart/Barchart.js
+++ src/components/barchart/Barchart.js
@@ -1,19 +1,21 @@
 /**
  * @author Shyam Soni
  * @description displaying the barchart from recharts
- * used the componentDidMount life cycle method to fetch the data from the backend 
- * from the dropdown menu we can select the Type which will show the count of the selected type 
+ * used the componentDidMount life cycle method to fetch the data from the backend
+ * from the dropdown menu we can select the Type which will show the count of the selected type
  * with there respective month
  */
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { client } from "../../actions/index";
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import ClipLoader from "react-spinners/ClipLoader";
 class Barchart extends Component {
   constructor(props) {
     super(props);
     this.state = {
       barChartData: [],
       selectedType: "A",
+      isLoading: true,
     };
   }
   handleSelect = (event) => {
@@ -28,6 +30,7 @@ class Barchart extends Component {
         console.log(response.data);
         this.setState({
           barChartData: response.data,
+          isLoading: false,
         });
       })
       .catch((error) => {
@@ -35,7 +38,7 @@ class Barchart extends Component {
       });
   }
   render() {
-    return (
+    const Barchart = (
       <div className="wrapper">
         <span className="row">Select Type</span>{" "}
         <select onChange={this.handleSelect}>
@@ -52,6 +55,14 @@ class Barchart extends Component {
           <Bar dataKey={this.state.selectedType} fill="#8884d8" />
         </BarChart>
       </div>
+    );
+    const Spinner = (
+      <div className="spinner">
+        <ClipLoader size={100} />
+      </div>
+    );
+    return (
+      <Fragment >{this.state.isLoading ? Spinner : Barchart}</Fragment>
     );
   }
 }
